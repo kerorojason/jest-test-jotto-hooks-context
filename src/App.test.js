5c6d1163374580f96f65ca713c616959d5bc733d
He Wang <r06631013@ntu.edu.tw>
2020-01-27 14:21:13 +0800

    basic app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testutils';
 import App from './App';
+import { exportAllDeclaration } from '@babel/types';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = (props = {}) => {
+  return shallow(<App />);
+};
+
+test('App renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
 });
