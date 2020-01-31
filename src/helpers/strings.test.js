6fb7563d26084abd0c3f6b28ad02451a9df898c5
He Wang <r06631013@ntu.edu.tw>
2020-01-31 20:11:43 +0800

    Strings wranings

--- src/helpers/strings.test.js
+++ src/helpers/strings.test.js
@@ -12,22 +12,40 @@ const strings = {
   mermish: {}
 };
 
-test('returns correct submit string for english', () => {
-  const string = getStringByLanguage('en', 'submit', strings);
-  expect(string).toBe('submit');
-});
+describe('language string testing', () => {
+  const mockWarn = jest.fn();
+  let originalWarn;
+  beforeEach(() => {
+    originalWarn = console.warn;
+    console.warn = mockWarn;
+  });
 
-test('returns correct submit string for emoji', () => {
-  const string = getStringByLanguage('emoji', 'submit', strings);
-  expect(string).toBe('🚀');
-});
+  afterEach(() => {
+    // avoid console.wran in future test being swallowed
+    console.warn = originalWarn;
+  });
 
-test('returns english submit string when language does not exist', () => {
-  const string = getStringByLanguage('noALanguage', 'submit', strings);
-  expect(string).toBe('submit');
-});
+  test('returns correct submit string for english', () => {
+    const string = getStringByLanguage('en', 'submit', strings);
+    expect(string).toBe('submit');
+    expect(mockWarn).not.toHaveBeenCalled();
+  });
 
-test('returns english submit string when submit key does not exist for language', () => {
-  const string = getStringByLanguage('mermish', 'submit', strings);
-  expect(string).toBe('submit');
+  test('returns correct submit string for emoji', () => {
+    const string = getStringByLanguage('emoji', 'submit', strings);
+    expect(string).toBe('🚀');
+    expect(mockWarn).not.toHaveBeenCalled();
+  });
+
+  test('returns english submit string when language does not exist', () => {
+    const string = getStringByLanguage('notALanguage', 'submit', strings);
+    expect(string).toBe('submit');
+    expect(mockWarn).toHaveBeenCalledWith('Could not get string [submit] for [notALanguage]');
+  });
+
+  test('returns english submit string when submit key does not exist for language', () => {
+    const string = getStringByLanguage('mermish', 'submit', strings);
+    expect(string).toBe('submit');
+    expect(mockWarn).toHaveBeenCalledWith('Could not get string [submit] for [mermish]');
+  });
 });
