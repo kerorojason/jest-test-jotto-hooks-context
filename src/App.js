1eb2fcdd3f6e66675c0fd066fe75474910f2c286
He Wang <r06631013@ntu.edu.tw>
2020-01-31 19:16:32 +0800

    Testing Hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import './App.css';
+import hookActions from './actions/hookActions';
+
+import Input from './Input';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return {
+        ...state,
+        secretWord: action.payload
+      };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
 
 function App() {
-  return <div data-test='component-app'></div>;
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = secretWord => {
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+  };
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  if (!state.secretWord) {
+    return (
+      <div className='container' data-test='component-spinner'>
+        <div className='spinner-border' role='status'>
+          <span className='sr-only'>Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className='container' data-test='component-app'>
+      <Input secretWord={state.secretWord}></Input>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,75 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testutils';
 import App from './App';
 
-const setup = (props = {}) => {
-  return shallow(<App />);
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
+const setup = (secretWord = 'party') => {
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn().mockReturnValue([{ secretWord }, jest.fn()]);
+  React.useReducer = mockUseReducer;
+
+  // use mount because useEffect is not called on ''shallow
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
 test('App renders without error', () => {
-  const wrapper = setup();
+  const wrapper = setup('party');
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on APp moint', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    // wrapper.update() doesn;t trigger useEffect update
+    // {issue forked from https://github.com/airbnb/enzyme/issues/2091}
+    wrapper.setProps();
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
+});
+
+describe('secretWord is not null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup('party');
+  });
+
+  test('renders app when secretWord is not null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.exists()).toBe(true);
+  });
+  test('does not render spinner when secretWord is not null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'component-spinner');
+    expect(spinnerComponent.exists()).toBe(false);
+  });
+});
+
+describe('secretWord is null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup(null);
+  });
+
+  test('does not render app when secretWord is null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.exists()).toBe(false);
+  });
+  test('render spinner when secretWord is null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'component-spinner');
+    expect(spinnerComponent.exists()).toBe(true);
+  });
 });
